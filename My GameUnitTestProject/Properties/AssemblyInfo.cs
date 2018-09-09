52538cc8676d5e3788ac73f63d750bd1af770e37
Abhi Sharma <abycoolnz05@gmail.com>
2018-09-09 15:49:24 +1200

    final commit

---
+++ My GameUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("My GameUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("My GameUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eee2db77-6363-40d5-8848-ced540c78f93")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ My GameUnitTestProject/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HangmanWordGame;
+using Android.Widget;
+using System.IO;
+using HangmanWordGame.Resources;
+
+namespace My_GameUnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public static int score;
+        public string letter;
+         
+        [TestMethod ]
+        public void TestMethod1()
+        {
+            switch (letter)
+            {
+                case "B":
+                case "C":
+                case "D":
+                case "F":
+                case "G":
+                case "H":
+                case "J":
+                case "K":
+                case "L":
+                case "M":   // All letters 1 pt 
+                case "N":
+                case "P":
+                case "Q":
+                case "R":
+                case "S":
+                case "T":
+                case "V":
+                case "W":
+                case "X":
+                case "Y":
+                case "Z":
+                    score = score + 1;
+                    break;
+            }
+            Assert.IsFalse(score == score + 1);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            switch (letter)
+            {
+                case "A":
+                case "E":  //All Wovels get 2 pts
+                case "I":
+                case "0":
+                    score = score + 2;
+                    break;
+            }
+            Assert.IsFalse(score == score + 2);
+        }
+    }
+}
